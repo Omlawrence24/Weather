6352acf19e386ce22e2cf3ee8a249a390f6481e6
Bebe Lawrence <omlawrence24@gmail.com>
2021-01-30 18:47:18 -0500

    history list

--- index.js
+++ index.js
@@ -1,91 +1,109 @@
-        
-        // 
-        // var forecast = $("#forecast").appendTo($(cardbody1)).text(response);
 
-        $(document).ready(function () {
-            $(".searchBtn").on("click", function(){
-            var searchValue = $("#search-value").val()
-            $("#search-value").val("")
-            getWeather(searchValue);
+// 
+// 
+$(document).ready(function () {
+    $(".searchBtn").on("click", function () {
+        var searchValue = $("#search-value").val()
+        $("#search-value").val("")
+        getWeather(searchValue);
+        createWeatherBtn(searchValue)
+    });
+ if (localStorage.getItem("history") != null) {
+JSON.parse(localStorage.getItem("history")).forEach(cityBtn => {
+    createWeatherBtn(cityBtn)
+})
 
-            });
-            
-            
-            
-            });
-            
-            
-            
-            function getWeather(searchValue) {
-            
-                $.ajax({
-                    url: "http://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=cd110b8f7f9c9303ea50c9e68d261cb3",
-                    method: "GET",
-                    dataType: "json",
-                }).then(function (response) {
-                    // console.log(response)
-                     storeHistory()
-                    //var searchValue = $(".search-value").val()
+ }
 
-                    
-                    var historyEntry = $("<button>").appendTo(historyList).text(searchValue)
-                    var weatherList = $("#weatherList").empty()
-                    var cityName = $("<h3>").appendTo(weatherList).text(response.city.name);
-                    var tempature = $("<p>").appendTo(weatherList).text("Temperature:  " + response.list[0].main.temp);
-                    var hummidity = $("<p>").appendTo(weatherList).text("Humidity:  " + response.list[0].main.humidity);
-                    var windSpeed = $("<p>").appendTo(weatherList).text("Wind Speed:  " + response.list[0].wind.speed + " MPH");
-                    var button = document.getElementById("historyList");
-                    button.onclick = function(){
-        
-                    //     var historySlot = searchValue.appendTo(weatherList)
-                    //  document.historySlot.append(weatherList)
-                    }
-                    
 
-                });
-            }
-             
-            function getUv() {
-                    var latt = uv.city.coord.lat
-                    var long = uv.city.coord.lon
-                    var urlUv = "http://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + latt + "&lon=" + long + "long&cnt={cnt}&appid=cd110b8f7f9c9303ea50c9e68d261cb3"
+});
 
-                $.ajax({
-                    url: urlUv,
-                    method: "GET",
-                    dataType: "json",
-                }).then(function (uv) {
-                    console.log(uv)
-                     var uvIndex = $("<p>").appendTo(weatherList).text(uv);
-                });
-            }
-            
-            function storeHistory() {
-                var searchValue = $("#search-value").val()
-                //var searchBtn = $(".searchBtn");
-                //var storeCity =  [];
-                 localStorage.setItem("History", searchValue);
-                 //alert(localStorage.getItem(searchValue))
-                 //storeCity.push(userCity)miami
-            //console.log(localStorage.getItem("History"));
-            }
-            // JSON.parse(localStorage.getItem("History")) ||
-            // for (let i = 0; i < userHistory + 1; i++) {
-                 
-            //     userhistory.appendTo($("#historyList"));
-            // }
-            
-            // function replacetext() {
-            // weatherList.replaceWith(userCity.attr({
-            //         id: '#weatherList',
-            //         value: userCity.text()
-            //     }));
-            
-            //    userCity.insertAfter("#weatherList");
-            
-            // } 
-            
-            // function historyList () {
-            // //     var historyList = [{""}]
-            
-            //     userCity.push(historyList){}
+ function createWeatherBtn (city) {
+  $("#historyList").append(`<button onclick="javascript:getWeather('${city}')" >${city}</button>`)   
+
+ }
+
+
+function getWeather(searchValue) {
+
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=cd110b8f7f9c9303ea50c9e68d261cb3",
+        method: "GET",
+        dataType: "json",
+    }).then(function (response) {
+        console.log(response);
+        storeHistory(searchValue);
+
+        forecast(response.city.coord.lat, response.city.coord.lon);
+       // $("<button>").appendTo(historyList).text(searchValue);
+        $("#weatherList").empty();
+        $("<h3>").appendTo(weatherList).text(response.city.name);
+        $("<p>").appendTo(weatherList).text("Temperature:  " + response.list[0].main.temp);
+        $("<p>").appendTo(weatherList).text("Humidity:  " + response.list[0].main.humidity);
+        $("<p>").appendTo(weatherList).text("Wind Speed:  " + response.list[0].wind.speed + " MPH");
+         
+    });
+}
+
+// $("#historyList").on("click", function () {
+//     getWeather()
+//     // var historySlot = searchValue.appendTo(weatherList)
+//     // document.historySlot.append(weatherList)
+// });
+
+
+
+function forecast(lat, lon) {
+    
+    var urlForecast = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=f14d9b76ec9372ef96ee61a166296635`;
+    $.ajax({
+        url: urlForecast,
+        method: "GET",
+        dataType: "json",
+    }).then(function (response) {
+        console.log("forecast data:");
+        console.log(response);
+       renderWeather(response)
+
+    });
+}
+
+
+function renderWeather (weatherData) {
+    $("#forecast-row").empty()
+  weatherData.daily.splice(0,5).forEach(weather => {
+      $("#forecast-row").append(`
+        <div class="col card text-white bg-primary mx-2" style="max-width: 18rem;">
+              <h4 class="card-header"> 1/13/2021 </h4>
+              <div id="forecast" class="card-body"> 
+                <h4 class="card-title"> sunny</h4>
+                <h3 class="card-text">Temp: ${weather.temp.day}</h3> 
+                <h3 class="card-text">Humidity: ${weather.humidity}</h3>
+              </div>
+              </p>
+          </div>
+    `)
+  })
+    
+    // $("<p>").appendTo(".card-header").text(weatherData.city)
+    // $("<p>").appendTo($("#forecast")).text(weatherData.daily[0].humidity)
+    // $("<p>").appendTo($("#forecast")).text(weatherData.daily[0].temp.day)
+    // $(forecastBox).appendTo($("#forecast"));
+
+}
+
+
+
+
+
+
+function storeHistory(city) {
+
+    //var searchBtn = $(".searchBtn");
+    var storeCity = localStorage.getItem("history") != null ? JSON.parse(localStorage.getItem("history")) : []
+    storeCity.push(city)
+    localStorage.setItem("history", JSON.stringify(storeCity));
+   
+           
+}
+    
